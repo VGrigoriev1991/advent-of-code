54e9d0da04a2764d0c486367a031ea6e9a3d39e5
Vladimir Grigoriev <vladimir.grigoriev@wickes.co.uk>
2025-02-02 20:58:18 +0200

    Implemented base solver infrastructure

---
+++ GW.AOC.Solvers/Year2024/Day1/Solver.cs
@@ -0,0 +1,16 @@
+﻿using GW.AOC.Contracts.Solvers;
+
+namespace GW.AOC.Solvers.Year2024.Day1;
+
+public class Solver : ISolver
+{
+    public Task SolvePartOneAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SolvePartTwoAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
