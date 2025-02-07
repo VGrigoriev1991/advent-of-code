7e6c3e2b5e7a22e954b357a0aa9d857b744e7824
Vladimir Grigoriev <vladimir.grigoriev@wickes.co.uk>
2025-02-07 18:17:33 +0200

    Added string extensions

---
+++ GW.AOC.Services/Extensions/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace GW.AOC.Services.Extensions;
+
+public static class StringExtensions
+{
+    public static int[] ToIntArray(this string source, string delimiter) =>
+        source.Split(delimiter, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+}
