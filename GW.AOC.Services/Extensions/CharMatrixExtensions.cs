32a3ee54288bc91d72cbbe8564a714c3d51f2faa
Vladimir Grigoriev <vladimir.grigoriev@wickes.co.uk>
2025-02-09 18:01:43 +0200

    Extended char matrix extensions

--- GW.AOC.Services/Extensions/CharMatrixExtensions.cs
+++ GW.AOC.Services/Extensions/CharMatrixExtensions.cs
@@ -14,6 +14,9 @@ public static class CharMatrixExtensions
         return false;
     }
 
+    public static bool IsValidPoint(this List<List<char>> source, Point point) =>
+        point is { X: >= 0, Y: >= 0 } && point.X < source.Count && point.Y < source[0].Count;
+
     public static List<Point> GetAllItemCoordinates(this List<List<char>> source, char symbol)
     {
         var result = new List<Point>();
@@ -25,6 +28,25 @@ public static class CharMatrixExtensions
                 if (source[i][j] == symbol)
                 {
                     result.Add(new Point(i, j));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static Point GetItemCoordinates(this List<List<char>> source, char symbol)
+    {
+        var result = new Point();
+
+        for (var i = 0; i < source.Count; i++)
+        {
+            for (var j = 0; j < source[0].Count; j++)
+            {
+                if (source[i][j] == symbol)
+                {
+                    result.X = i;
+                    result.Y = j;
                 }
             }
         }
