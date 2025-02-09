5ebb6f22241e28520ddc9b0360de0325a61872cf
Vladimir Grigoriev <vladimir.grigoriev@wickes.co.uk>
2025-02-09 18:13:36 +0200

    Added implementation of second puzzle of the day - 6 year - 2024

--- GW.AOC.Solvers/Year2024/Day6/Solver.cs
+++ GW.AOC.Solvers/Year2024/Day6/Solver.cs
@@ -1,4 +1,5 @@
-﻿using GW.AOC.Contracts.Models;
+﻿using System.Drawing;
+using GW.AOC.Contracts.Models;
 using GW.AOC.Contracts.Services;
 using GW.AOC.Contracts.Solvers;
 using GW.AOC.Services.Extensions;
@@ -18,14 +19,62 @@ public class Solver(IPuzzleDataReader puzzleDataReader) : SolverBase, ISolver
         return Task.CompletedTask;
     }
 
-    public Task SolvePartTwoAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    public Task SolvePartTwoAsync(CancellationToken cancellationToken)
+    {
+        var data = puzzleDataReader.ReadCharMatrix(PuzzleDataFilePath);
 
-    private static HashSet<string> GetVisitedPoints(List<List<char>> matrix)
+        var visitedPoints = GetVisitedPoints(data);
+
+        var result = 0L;
+
+        foreach (var visitedPoint in visitedPoints.Skip(1))
+        {
+            var startPoint = visitedPoints.First();
+            var currentDirection = PointDirection.Top;
+            var newlyVisitedPoints = new HashSet<(Point, Point)> { (startPoint, currentDirection) };
+
+            while (true)
+            {
+                var nextPoint = startPoint.GetNext(currentDirection);
+                if (!data.IsValidPoint(nextPoint))
+                {
+                    break;
+                }
+
+                if ((data.ContainsItem(Symbol.Dot, nextPoint) || data.ContainsItem(Symbol.Caret, nextPoint))
+                    && nextPoint != visitedPoint)
+                {
+                    startPoint = nextPoint;
+
+                    var newlyVisitedPoint = (startPoint, currentDirection);
+
+                    if (newlyVisitedPoints.Add(newlyVisitedPoint))
+                    {
+                        continue;
+                    }
+
+                    result++;
+                    break;
+                }
+
+                if (data.ContainsItem(Symbol.Sharp, nextPoint) || nextPoint == visitedPoint)
+                {
+                    currentDirection = currentDirection.GetNextMainDirection();
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+
+        return Task.CompletedTask;
+    }
+
+    private static HashSet<Point> GetVisitedPoints(List<List<char>> matrix)
     {
         var startPoint = matrix.GetItemCoordinates(Symbol.Caret);
 
         var currentDirection = PointDirection.Top;
-        var visitedPoints = new HashSet<string> { startPoint.ToString() };
+        var visitedPoints = new HashSet<Point> { startPoint };
 
         while (true)
         {
@@ -38,7 +87,7 @@ public class Solver(IPuzzleDataReader puzzleDataReader) : SolverBase, ISolver
             if (matrix.ContainsItem(Symbol.Dot, nextPoint) || matrix.ContainsItem(Symbol.Caret, nextPoint))
             {
                 startPoint = nextPoint;
-                visitedPoints.Add(startPoint.ToString());
+                visitedPoints.Add(startPoint);
             }
             else if (matrix.ContainsItem(Symbol.Sharp, nextPoint))
             {
